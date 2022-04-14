6823eabf6a1667d81e15d88002810f9823e4a8b5
suninfire <n.suninfire@gmail.com>
2022-04-14 17:53:15 +0300

    hw3

---
+++ js/hw1/main.js
@@ -0,0 +1,96 @@
+// let a = 'hello';
+// let b = 'owu';
+// let c = 'com';
+// let d = 'ua';
+// let e = 1;
+// let f = 10;
+// let g = -999;
+// let h = 123;
+// let i = 3.14;
+// let j = 2.7;
+// let k = 16;
+// let l = true;
+// let m = false;
+// console.log(a, b, c, d, e, f, g, h, i, j, k, l, m);
+// alert(a, b, c, d, e, f, g, h, i, j, k, l, m);
+// document.write(a, b, c, d, e, f, g, h, i, j, k, l, m);
+
+// let book = {
+//     name: 'joyland',
+//     pages: 300,
+//     genre: 'horror'
+// };
+
+
+// let book2 = {
+//     name: 'shining',
+//     pages: 600,
+//     genre: 'horror',
+//     author: ['Stephen King','zxv','asad']
+// };
+
+
+// let firstName = 'Nataliia';
+// let middleName = 'Uriivna';
+// let lastName = 'Kamkova';
+// let person = firstName + ' ' + middleName + ' ' + lastName;
+// console.log(person)
+
+
+// let name = prompt('your name');
+// let middle = prompt('your middle name');
+// let age = prompt('your age');
+// console.log(name, middleName, +age)
+
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+
+// let book = [
+//     {name: 'the shining', pages: 600, genre: 'horror', author: 'Stephen'},
+//     {name: 'joyland', pages: 300, genre: 'fantasy', author: 'King'},
+//     {name: 'it', pages: 100, genre: 'triller', author: 'Stephen King'},
+// ]
+// console.log(book[1],book[0],book[2])
+
+// 5.
+let height = 23;
+let width = 10;
+let s = height*width;
+console.log(s);
+
+// 6.
+let heightC = 10;
+let dC = 4;
+const p = 3.14;
+let vC = p*dC*heightC
+console.log(vC);
+
+// 7.
+let n = 3;
+let m = 4;
+let k = Math.sqrt(Math.pow(n,2) + Math.pow(m,2));
+console.log(k);
+
+// 8
+
+let a = 5 < 6;
+console.log(a);
+
+let b = 5 > 6;
+let c = 5 == 6;
+let d = 5 === 6;
+console.log(b,c,d)
+
+let e = [10 == 10, 10 === 10, 10 < 10, 10 > 10, 10 != 10]
+console.log(e);
+
+let f = 123 === '123';
+let g = 123 == '123';
+console.log(f, g);
+
