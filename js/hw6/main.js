640b14dc39f9b037d58715f595eff83e5bd339e1
suninfire <n.suninfire@gmail.com>
2022-05-07 17:43:39 +0300

    hw3

--- js/hw6/main.js
+++ js/hw6/main.js
@@ -79,16 +79,73 @@
 // console.log(filter);
 
 
-// описати колоду карт
+// описати колоду карт  піка    буба      черва   хреста
+// {cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
+// value: '', // '6'-'10', 'ace','jack','queen','king','joker'
+// color:'', // 'red','black'}
+
+// let deck = [
+//     {cardSuit: 'spade', value: '6', color: 'black'},
+//     {cardSuit: 'spade', value: '7', color: 'black'},
+//     {cardSuit: 'spade', value: '8', color: 'black'},
+//     {cardSuit: 'spade', value: '9', color: 'black'},
+//     {cardSuit: 'spade', value: '10', color: 'black'},
+//     {cardSuit: 'spade', value: 'jack', color: 'black'},
+//     {cardSuit: 'spade', value: 'queen', color: 'black'},
+//     {cardSuit: 'spade', value: 'king', color: 'black'},
+//     {cardSuit: 'spade', value: 'ace', color: 'black'},
+//
+//     {cardSuit: 'clubs', value: '6', color: 'black'},
+//     {cardSuit: 'clubs', value: '7', color: 'black'},
+//     {cardSuit: 'clubs', value: '8', color: 'black'},
+//     {cardSuit: 'clubs', value: '9', color: 'black'},
+//     {cardSuit: 'clubs', value: '10', color: 'black'},
+//     {cardSuit: 'clubs', value: 'jack', color: 'black'},
+//     {cardSuit: 'clubs', value: 'queen', color: 'black'},
+//     {cardSuit: 'clubs', value: 'king', color: 'black'},
+//     {cardSuit: 'clubs', value: 'ace', color: 'black'},
+//
+//     {cardSuit: 'diamond', value: '6', color: 'red'},
+//     {cardSuit: 'diamond', value: '7', color: 'red'},
+//     {cardSuit: 'diamond', value: '8', color: 'red'},
+//     {cardSuit: 'diamond', value: '9', color: 'red'},
+//     {cardSuit: 'diamond', value: '10', color: 'red'},
+//     {cardSuit: 'diamond', value: 'jack', color: 'red'},
+//     {cardSuit: 'diamond', value: 'queen', color: 'red'},
+//     {cardSuit: 'diamond', value: 'king', color: 'red'},
+//     {cardSuit: 'diamond', value: 'ace', color: 'red'},
+//
+//     {cardSuit: 'heart', value: '6', color: 'red'},
+//     {cardSuit: 'heart', value: '7', color: 'red'},
+//     {cardSuit: 'heart', value: '8', color: 'red'},
+//     {cardSuit: 'heart', value: '9', color: 'red'},
+//     {cardSuit: 'heart', value: '10', color: 'red'},
+//     {cardSuit: 'heartd', value: 'jack', color: 'red'},
+//     {cardSuit: 'heart', value: 'queen', color: 'red'},
+//     {cardSuit: 'heart', value: 'king', color: 'red'},
+//     {cardSuit: 'heart', value: 'ace', color: 'red'},
+//
+//     {value: 'joker', color: 'black'},
+//     {value: 'joker', color: 'red'}
+//
+// ];
+
 // - знайти піковий туз
+//let spadeace = deck.find((item) => item.cardSuit === 'spade' && item.value === 'ace' );
+//console.log(spadeace);
+
 // - всі шістки
+//let six = deck.filter((item) => item.value === '6');
+//console.log(six);
+
 // - всі червоні карти
+//let reds = deck.filter((item) => item.color === 'red');
+//console.log(reds);
+
 // - всі буби
+//let diamonds = deck.filter((item) => item.cardSuit === 'diamond');
+//console.log(diamonds);
+
 // - всі трефи від 9 та більше
-//
-//
-// {
-//     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
-//         value: '', // '6'-'10', 'ace','jack','queen','king','joker'
-//     color:'', // 'red','black'
-// }
+//let clubs = deck.filter((item) => item.cardSuit === 'clubs' && item.value >= '9');
+//console.log(clubs);
